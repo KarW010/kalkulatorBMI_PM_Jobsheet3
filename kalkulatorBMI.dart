11089670376484b47cb39baaaabc57cf391a48ca
KarW010 <kartikostudy@gmail.com>
2026-03-10 11:56:17 +0700

    Menambahkan parameter dan fitur penyimpanan riwayat

--- kalkulatorBMI.dart
+++ kalkulatorBMI.dart
@@ -1,8 +1,15 @@
 void main() {
-
+  List<Map<String, dynamic>> riwayat = [];
 }
 
-void hitungBMI(double tinggiCm, double beratKg) {
+void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
   double tinggiM = tinggiCm / 100;
   double bmi = beratKg / (tinggiM * tinggiM);
+
+  riwayat.add({
+    'tinggi': tinggiCm,
+    'berat': beratKg,
+    'bmi': bmi,
+    'kategori': kategori
+  });
 }
